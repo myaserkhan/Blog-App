24d23884ff6ab4f41becdafc6213ec1bc46a6017
Yasir Khan <khanyaser007@gmail.com>
2023-01-31 21:11:27 +0500

    Add foreign key & corresponding index to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_141053) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_160844) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_141053) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.bigint "post_id"
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -43,4 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_141053) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
 end
