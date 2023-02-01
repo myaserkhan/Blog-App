ae3339700f7df447868efb93a0d0e3f261109caf
Yasir Khan <khanyaser007@gmail.com>
2023-02-01 14:30:21 +0500

    Create a method to return 5 recent comments for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   has_many :comments, class_name: "Comment", foreign_key: "post_id"
   has_many :likes, class_name: "Like", foreign_key: "post_id"
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
