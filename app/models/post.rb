9354bc60e973c24d36cc3d20afb71ba93a404180
Yasir Khan <khanyaser007@gmail.com>
2023-02-01 14:30:55 +0500

    Create a method to increment and save posts_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,8 @@ class Post < ApplicationRecord
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
+  
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
