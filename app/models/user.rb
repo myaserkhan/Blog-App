c72aa344b6e2ddcb678e776098999df67d855283
Yasir Khan <khanyaser007@gmail.com>
2023-02-02 16:46:15 +0500

    Add min and max length for user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
 
   validates :name, presence: true
+  validates :name, presence: true, length: { in: 3..25 }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
