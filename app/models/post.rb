2e3cc7c63abb298ed39719d7f73aa8234f05336f
Yasir Khan <khanyaser007@gmail.com>
2023-02-02 16:48:18 +0500

    Add validatons for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,11 @@ class Post < ApplicationRecord
 
   after_save :update_posts_counter
 
+  validates :text, presence: true
+  validates :title, presence: true, length: { in: 3..250 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
