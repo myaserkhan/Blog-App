20fb9c64a4c82aae26f5c0ffd05084683a1ed6da
Yasir Khan <khanyaser007@gmail.com>
2023-02-02 16:40:05 +0500

    Create Mig to set posts_counter default value

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
 
-  validates :name, presence: true 
+  validates :name, presence: true
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
