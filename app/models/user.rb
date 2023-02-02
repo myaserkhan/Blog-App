501685cc30ce68ab95f14c80bb023e180f41de2d
Yasir Khan <khanyaser007@gmail.com>
2023-02-02 16:26:38 +0500

    Add validation for posts_counter in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
+    validates :PostsCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   end
 end
