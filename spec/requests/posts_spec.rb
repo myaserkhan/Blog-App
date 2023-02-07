23235c52f084f8064a73c3e40f8594dc79faa595
Yasir Khan <khanyaser007@gmail.com>
2023-02-07 13:17:13 +0500

    Add tests for index action in PostssController

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'PostsController', type: :request do
+  user = User.create(name: 'Tom & Jerry', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Best friends', PostsCounter: 12)
+  subject do
+    Post.new(title: 'Physics', text: 'This is not my first post', comments_counter: 10, likes_counter: 10,
+             author: user)
+  end
+
+  before { subject.save } 
+
+  describe "GET #index" do
+    before(:each) { get user_posts_path(user_id: 1) }
+    it "returns http success" do
+      # get '/users/1/posts'
+      # get user_posts_path(1)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the correct template" do
+      # get '/users/1/posts'
+      # get user_posts_path(user_id: 1)
+      expect(response).to render_template(:index)
+    end
+
+    it "includes the correct placeholder text in the response body" do
+      # get '/users/1/posts'
+      # get user_posts_path(user_id: 1)
+      expect(response.body).to include("Hello from user's all posts page!")
+    end
+  end
+end
