fd780466298b45d5b2e042ebce52551151f56a56
Yasir Khan <khanyaser007@gmail.com>
2023-02-07 13:56:43 +0500

    Remove commented code

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -13,20 +13,14 @@ RSpec.describe 'PostsController', type: :request do
   describe 'GET #index' do
     before(:each) { get user_posts_path(user_id: 1) }
     it 'returns http success' do
-      # get '/users/1/posts'
-      # get user_posts_path(1)
       expect(response).to have_http_status(:ok)
     end
 
     it 'renders the correct template' do
-      # get '/users/1/posts'
-      # get user_posts_path(user_id: 1)
       expect(response).to render_template(:index)
     end
 
     it 'includes the correct placeholder text in the response body' do
-      # get '/users/1/posts'
-      # get user_posts_path(user_id: 1)
       expect(response.body).to include("Hello from user's all posts page!")
     end
   end
@@ -34,20 +28,14 @@ RSpec.describe 'PostsController', type: :request do
   describe 'GET #show' do
     before(:each) { get user_post_path(user_id: 1, id: 1) }
     it 'returns http success' do
-      # get '/users/1/posts/1'
-      # get user_post_path(user_id: 1, id: 1)
       expect(response).to have_http_status(:ok)
     end
 
     it 'renders the correct template' do
-      # get '/users/1/posts/1'
-      # get user_post_path(user_id: 1, id: 1)
       expect(response).to render_template(:show)
     end
 
     it 'includes the correct placeholder text in the response body' do
-      # get '/users/1/posts/1'
-      # get user_post_path(user_id: 1, id: 1)
       expect(response.body).to include('Hello from a selected post page!')
     end
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,22 +6,16 @@ RSpec.describe 'UsersController', type: :request do
   before { subject.save }
 
   describe 'GET #index' do
-    before(:example) { get users_path } # get(:index)
+    before(:example) { get users_path }
     it 'returns http success' do
-      # get '/users'
-      # get users_path
       expect(response).to have_http_status(:ok)
     end
 
     it 'renders the correct template' do
-      # get '/users'
-      # get users_path
       expect(response).to render_template(:index)
     end
 
     it 'includes the correct placeholder text in the response body' do
-      # get '/users'
-      # get users_path
       expect(response.body).to include('Hello from all users page!')
     end
   end
@@ -30,20 +24,14 @@ end
 describe 'GET #show' do
   before(:example) { get user_path(id: 1) }
   it 'returns http success' do
-    # get '/users/1'
-    # get user_path(id: 1)
     expect(response).to have_http_status(:ok)
   end
 
   it 'renders the correct template' do
-    # get '/users/1'
-    # get user_path(id: 1)
     expect(response).to render_template(:show)
   end
 
   it 'includes the correct placeholder text in the response body' do
-    # get '/users/1'
-    # get user_path(id: 1)
     expect(response.body).to include('Hello from a selected user page!')
   end
 end
