3993aff53c1ad1290bbf2a9b78a69e51fb72d442
Yasir Khan <khanyaser007@gmail.com>
2023-02-07 13:14:48 +0500

    Test show action in UsersController

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -26,3 +26,24 @@ RSpec.describe 'UsersController', type: :request do
     end
   end
 end
+
+describe "GET #show" do
+  before(:example) { get user_path(id: 1) }
+  it "returns http success" do
+    # get '/users/1'
+    # get user_path(id: 1)
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "renders the correct template" do
+    # get '/users/1'
+    # get user_path(id: 1)
+    expect(response).to render_template(:show)
+  end
+
+  it "includes the correct placeholder text in the response body" do
+    # get '/users/1'
+    # get user_path(id: 1)
+    expect(response.body).to include("Hello from a selected user page!")
+  end
+end
