8a4855733dd1a48f9f768e5976ae6abf659ef80d
Yasir Khan <khanyaser007@gmail.com>
2023-02-08 13:29:57 +0500

    Temporary remove pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,7 @@
 class PostsController < ApplicationController
   def index
-    offset = params[:offset] ? params[:offset].to_i : 0
-    limit = 5
     @user = User.find(params[:user_id])
-    @posts = @user.posts.order(created_at: :asc).limit(5).offset(offset)
-    @total_posts = @user.posts.count
-    @total_pages = (@total_posts.to_f / limit.to_f).ceil
-    @current_page = (offset / limit )
+    @posts = @user.posts.order(created_at: :asc)
   end
 
   def show
