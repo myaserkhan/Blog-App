a9cff06335a5fa1e65ca6c6aefb4253f6da9095b
Yasir Khan <khanyaser007@gmail.com>
2023-02-11 19:42:04 +0500

    Implement error handling for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,13 +8,12 @@ class CommentsController < ApplicationController
     @comment.author = current_user
     @comment.post = Post.find(params[:post_id])
 
-    respond_to do |_format|
-      if @comment.save
+    if @comment.save
 
-        redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id])
-      else
-        render :new
-      end
+      redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id]), notice: 'Comment added successfully'
+    else
+      flash.now[:alert] = @post.errors.full_messages.first if @post.errors.any?
+      render :new, status: 400
     end
   end
 
