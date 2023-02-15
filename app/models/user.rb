66cb9cc4e8b74d27c06dac74b903faa2d18b05c3
Gilbert <okonjogilbert2@gmail.com>
2023-02-15 11:37:25 +0300

    Add confirmable to devise for confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+  
   has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
 
-  validates :name, presence: true, length: { in: 3..25 }
-  validates :PostsCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :photo, presence: true
-  validates :bio, presence: true
+  validates :name, presence: true
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :photo, presence: true
+  # validates :bio, presence: true
+
+  # has_one_attached :photo
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
