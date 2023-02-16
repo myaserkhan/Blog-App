446c589e02bd0ccc4ea39c51f2fe6bde051562d0
Harrynoqs <noqsnoqs@gmail.com>
2023-02-16 11:14:06 +0300

    Load and authorize resources

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
   before_action :set_post, only: %i[new create]
 
   def new
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
   before_action :find_user, only: %i[index new show]
 
   def index
