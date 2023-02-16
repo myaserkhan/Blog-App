97d0b97965123e3b98f7d5be3fa03a4b1ddb4149
Gilbert <okonjogilbert2@gmail.com>
2023-02-16 11:36:01 +0300

    Add an action and route to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,6 +19,17 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+
+    if @comment.destroy
+      redirect_to user_post_path(user_id: current_user.id, id: @comment.post_id), notice: 'Comment deleted successfully'
+    else
+      flash.new[:alert] = @comment.errors.full_messages.first if @comment.errors.any?
+      render :show, status: 400
+    end
+  end
+  
   private
 
   def set_post
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
-    resources :comments, only: [:new, :create]
+    resources :comments, only: [:new, :create, :destroy]
     resources :likes, only: [:create]
   end
   end
